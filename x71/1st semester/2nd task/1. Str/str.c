123f49937069bd82925633bf1d4f2323d27ea380
Arem Gorohov <gorohov.art@gmail.com>
2013-10-15 17:22:34 +0400

    fixed strcmp

--- x71/1st semester/2nd task/1. Str/str.c
+++ x71/1st semester/2nd task/1. Str/str.c
@@ -37,9 +37,22 @@ void strcat(char *s1, char *s2)
 
 int strcmp(char *s1, char *s2)
 {
-	int len1 = strlen(s1);
+    int len1 = strlen(s1);
 	int len2 = strlen(s2);
-	
+    int min = len1;
+    int i = 0;
+
+    if (len1 > len2)
+        min = len2;
+    	
+    for (i = 0; i < min; i++)
+    {
+        if (s1[i] < s2[i])
+            return -1;
+        if (s1[i] > s2[i])
+            return 1;
+    }
+
 	if (len1 < len2) 
 		return -1;
 	if (len1 == len2) 
