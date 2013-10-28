b79e71b15bcbe7ce70d3e04489f23a73d21702ec
Artem Gorohov <gorohov.art@gmail.com>
2013-10-28 18:00:42 +0400

    Added signature for isPrime

--- x71/1st semester/1st task/02. isPrime/main.c
+++ x71/1st semester/1st task/02. isPrime/main.c
@@ -14,7 +14,9 @@ int isPrime(int n)
     for (i = 2; i * i < n; i++)
     {
         if (n % i == 0)
+        {
             return 0;
+        }
     }
 
     return 1;
@@ -24,12 +26,17 @@ int main()
 {
     int n = 0;
 
-    printf("input number: ");
+    printf("Input number: ");
     scanf("%d", &n);
 
     if (isPrime(n))
+    {
         printf("Your number is prime.\n");
-    else printf("Your number is not prime.\n");
+    }
+    else
+    {
+    	printf("Your number is not prime.\n");
+    }
 
     return 0;
 }
