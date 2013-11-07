8605f986d4f366e26a2dd51b04511da5780ecba9
Artem Gorohov <gorohov.art@gmail.com>
2013-11-08 00:47:04 +0400

    Added brackets and signature

--- x71/1st semester/2nd task/3. Table out/main.c
+++ x71/1st semester/2nd task/3. Table out/main.c
@@ -1,3 +1,9 @@
+/*
+Artem Gorokhov (c) 2013
+
+Homework 2
+Pascal's "triangle"
+*/
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -22,7 +28,9 @@ void outLine(int n, int maxLen)
     {
         printf("+");
         for (j = 0; j < maxLen; j++)
+        {
             printf("-");
+        }
     }
     printf("+\n");
 }
@@ -33,51 +41,65 @@ void outCell(int number, int maxLen)
 
     printf("|%d", number);
     for (i = 0; i < maxLen - numLen(number); i++)
+    {
         printf(" ");
+    }
 }
 
-
-int main ()
+void outTable(int n)
 {
-	char s1[1000];
-	char s2[1000];
-    int n = 0;
+    char s1[1000];
+    char s2[1000];
     int i = 0;
     int j = 0;
     int maxLen = 0;
     int **array;
 
-    printf("Input n: ");
-    scanf("%d", &n);
-	
-    array = (int **) malloc(sizeof(int) * (n + 1));
-
-	for (i = 0; i <= n; i++)
-	{
-	    array[i] = (int *)malloc(sizeof(int) * (n + 1));
-	}
-
+    array = (int **) malloc(sizeof(int*) * (n + 1));
+    for (i = 0; i <= n; i++)
+    {
+        array[i] = (int *)malloc(sizeof(int) * (n + 1));
+    }
+    
     for (i = 1; i <= n; i++)
+    {
         for (j = 1; j <= n; j++)
+        {
             if (i == 1 || j == 1)
+            {
                 array[i][j] = 1;
+            }
             else
+            {
                 array[i][j] = array[i - 1][j] + array[i][j - 1];
-
+            }
+        }
+    }
 
     maxLen = numLen(array[n][n]);
-
+    
     for (i = 1; i <= n; i++)
     {
         outLine(n, maxLen);
 
         for (j = 1; j <= n; j++)
+        {
             outCell(array[i][j], maxLen);
+        }
         printf("|\n");
     }
     outLine(n, maxLen);
-
+    
     free(array);
-    system ("pause");
-	return 0;
+}
+
+int main ()
+{
+    int n = 0;
+    
+    printf("Input n: ");
+    scanf("%d", &n);
+
+    outTable(n);
+    return 0;
 }
