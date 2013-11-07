eb0d815d347ac2e7904410f3dca114af57a85ae6
Artem Gorohov <gorohov.art@gmail.com>
2013-11-08 00:44:42 +0400

    Added function

---
+++ x71/1st semester/1st task/03. Palindrome/main.c
@@ -0,0 +1,88 @@
+/*
+Artem Gorokhov (c) 2013
+
+Homework 1
+String is palindrome?
+*/
+#include <string.h>
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+int isEqual(char a, char b)
+{
+    const int comp = 'a' - 'A';
+
+    if ((a == b) || (fabs(a - b) == comp))
+    {
+        return 1;
+    }
+    return 0;
+}
+
+int isPalindrome(char *s)
+{
+    int lens = strlen(s);
+    int a = 0;
+    int b = 0;
+    
+    b = lens - 1;
+    if (s[b] < 30)
+    {
+        b--;
+    }
+
+    while (a <= b)
+    {
+        while (s[a] == ' ')
+        {
+            a++;
+            if (a == lens)
+            {
+                break;
+            }
+        }
+        while (s[b] == ' ')
+        {
+            b--;
+            if (b < 0)
+            {
+                break;
+            }
+        }
+
+        if (a > b)
+        {
+            break;
+        }
+
+        if (isEqual(s[a], s[b]) == 0)
+        {
+            return 0;
+        }
+        a++;
+        b--;
+
+    }
+
+    return 1;
+}
+
+int main()
+{
+    char s[200];
+
+
+    printf("input string : ");
+    fgets(s, 200, stdin);
+    
+    if (isPalindrome(s))
+    {
+        printf("String is palindrome\n");
+    }
+    else 
+    {
+        printf("String is not palindrome\n");
+    }
+    return 0;
+}
