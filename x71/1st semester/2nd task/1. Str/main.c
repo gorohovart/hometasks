fa5049a53f1d71e5e9e57e515b13fa7652c43bcd
Artem Gorohov <gorohov.art@gmail.com>
2013-11-08 00:45:52 +0400

    Changed functions cpy, len, cmp

--- x71/1st semester/2nd task/1. Str/main.c
+++ x71/1st semester/2nd task/1. Str/main.c
@@ -1,20 +1,21 @@
+/*
+Artem Gorokhov (c) 2013
+
+Homework 2
+String library check.
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include "str.h"
 
 int main ()
 {
-	char s1[1000];
-	char s2[1000];
-	char s3[1000];
+    char s1[] = {"asd"};
+    char s2[] = {"qwerty"};
+    char s3[15];
 
-    printf("input s1: ");
-    fgets(s1, 200, stdin);
-
-    printf("input s2: ");
-	fgets(s2, 200, stdin);
-
-	printf("\nstrlen(s1) = %d\nstrlen(s2) = %d\n", strlen(s1), strlen(s2));
+    printf("s1: %s\ns2: %s\n", s1, s2);
+    printf("strlen(s1) = %d\nstrlen(s2) = %d\n", strlen(s1), strlen(s2));
 
     strcpy(s3, s1);
     printf("s3 = s1 = %s\n", s3);
@@ -24,6 +25,5 @@ int main ()
 
     printf("s1 > s2? %d\n", strcmp(s1, s2));
 
-    system ("pause");
-	return 0;
+    return 0;
 }
--- x71/1st semester/2nd task/1. Str/str.c
+++ x71/1st semester/2nd task/1. Str/str.c
@@ -1,62 +1,80 @@
+/*
+Artem Gorokhov (c) 2013
+
+Homework 2
+String library.
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include "str.h"
 
 int strlen(char *string)
 {
-	int i = 0;
+    int i = 0;
 
-	while (string[i] != 0 && string[i] != 10)
-		i++;
-	return i;
+    while (string[i] != 0)
+    {
+        i++;
+    }
+    return i;
 }
 
 void strcpy(char *s1, char *s2)
 {
-	int len2 = strlen(s2);
-
     int i = 0;
-	for (i = 0; i < len2; i++)
-		s1[i] = s2[i];
 
-    s1[len2] = 0;
+    while (s2[i] != 0)
+    {
+        s1[i] = s2[i];
+        i++;
+    }
+
+    s1[i] = 0;
 }
 
 void strcat(char *s1, char *s2)
 {
-	int len1 = strlen(s1);
-	int len2 = strlen(s2);
-    int i = 0;
+    const int len1 = strlen(s1);
+    int i = len1;
 
-	for (i = len1; i < len1 + len2; i++)
-		s1[i] = s2[i - len1];
-
-    s1[len1 + len2] = 0;
+    while(s2[i - len1] != 0)
+    {
+        s1[i] = s2[i - len1];
+        i++;
+    }
+    
+    s1[i] = 0;
 }
 
 
 int strcmp(char *s1, char *s2)
 {
-    int len1 = strlen(s1);
-	int len2 = strlen(s2);
-    int min = len1;
     int i = 0;
 
-    if (len1 > len2)
-        min = len2;
-    	
-    for (i = 0; i < min; i++)
+    while(s1[i] != 0 && s2[i] != 0)
     {
         if (s1[i] < s2[i])
+        {
             return -1;
+        }
         if (s1[i] > s2[i])
+        {
             return 1;
+        }
+
+        i++;
     }
 
-	if (len1 < len2) 
-		return -1;
-	if (len1 == len2) 
-		return 0;
-	if (len1 > len2) 
-		return 1;
+    if (s1[i] < s2[i])
+    {
+        return -1;
+    }
+    if (s1[i] == s2[i]) 
+    {
+        return 0;
+    }
+    if (s1[i] > s2[i]) 
+    {
+        return 1;
+    }
 }
