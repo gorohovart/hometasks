3b9215d243c5dcf4783743157f030dbe694755fb
Artem Gorohov <gorohov.art@gmail.com>
2013-11-08 00:49:39 +0400

    Added console args

--- x71/1st semester/4th task/2. Head/main.c
+++ x71/1st semester/4th task/2. Head/main.c
@@ -1,40 +1,31 @@
+/*
+Artem Gorokhov (c) 2013
+
+Homework 4
+Head
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-void inParams(int *n, char *name)
-{
-    char c[1000];
-    scanf("%s", c);
-
-    if (c[0] == '?')
-        fgets(c, 1000, stdin);//to next line
-    else if (c[0] == '-')
-        scanf("%d", n);
-    else
-    {
-        strcpy(name, c);
-        return;
-    }
-
-    scanf("%s", name);
-    fgets(c, 100, stdin);//to next line
-}
-
-void stdRead(int n)
+void stdReadWrite(int n)
 {
     char string[100][1000];
     int i = 0;
 
     for (i = 0; i < n; i++)
+    {
         fgets(string[i], 1000, stdin);
+    }
 
     printf("\n");
     for (i = 0; i < n; i++)
+    {
         printf("%s", string[i]);
+    }
 }
 
-void fileRead(char *name, int n)
+void fileReadWrite(char *name, int n)
 {
     char string[100][1000];
     int i = 0;
@@ -54,24 +45,38 @@ void fileRead(char *name, int n)
     }
 
     for (i = 0; i < last; i++)
-    printf("%s", string[i]);
+    {
+        printf("%s", string[i]);
+    }
 }
 
-int main()
+int main(int argc, char **argv)
 {
     int readFromStd = 1;
     int n = 10;
     int i = 0;
     char name[100];
 
-
-    printf("Head.\nInput -n <numOfLines>(optional) nameOfFile(optional) (in single line)\nIf no parameters, in '?'\n");
-    inParams(&n, name);
-
-    if (name[0] > 33)
-        fileRead(name, n);
+    for (i = 1; i < argc; i++)
+    {
+        if (strcmp(argv[i], "-n") == 0)
+        {
+            sscanf(argv[i + 1], "%d", &n);
+            i++;
+        }
+        else
+        {
+            readFromStd = 0;
+            strcpy(name, argv[i]);
+        }
+    }
+    
+    if (readFromStd)
+    {
+        stdReadWrite(n);
+    }
     else
-        stdRead(n);
-
-    system("pause");
+    {
+        fileReadWrite(name, n);
+    }
 }
