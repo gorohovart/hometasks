dca65e24f1ea7864c525d4196e8b616a48237ee8
Arem Gorohov <gorohov.art@gmail.com>
2013-11-08 12:11:33 +0400

    Added printBin function

--- x71/1st semester/3rd task/1. Binary/main.c
+++ x71/1st semester/3rd task/1. Binary/main.c
@@ -1,26 +1,41 @@
+/*
+Artem Gorokhov (c) 2013
+
+Homework 3
+Decimal to binary
+*/
 #include <stdio.h>
 #include <stdlib.h>
 
-int main ()
+void printBin(int n)
 {
-	int n = 0;
-    int flag = 0;
     int i = 0;
+    int flag = 0;
 
-    printf("Input n: ");
-    scanf("%d", &n);
     printf("Binary: ");
-
     for (i = 31; i >= 0; i--)
     {
         if (flag == 0 && (n >> i) & 1 == 1)
+        {
             flag = 1;
-        
+        }
         if (flag == 1)
+        {
             printf("%d", (n >> i) & 1);
+        }
     }
 
     printf("\n");
+}
+
+int main ()
+{
+    int n = 0;
+    
+    printf("Input n: ");
+    scanf("%d", &n);
+    printBin(n);
+
     system ("pause");
-	return 0;
+    return 0;
 }
