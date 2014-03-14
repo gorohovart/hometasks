cd5fa5e62c2d980cdec6d059538f62cd606b316d
gorohovart <gorohov.art@gmail.com>
2014-03-14 23:14:08 +0400

    Added sumFib

---
+++ x71/2nd semester/Task 2/Class/2. sumFib/Program.fs
@@ -0,0 +1,13 @@
+﻿let max = 4L * 1000L * 1000L
+
+let fibonachiSum = 
+    let rec fib a b = 
+        if b >= max then
+            0L
+        else if (a + b) % 2L = 0L then
+            (a + b) + fib b (a + b)
+        else
+            fib b (a + b)
+    fib 1L 1L
+
+printfn "%A" fibonachiSum
