dd2cf1a441fc16e4c16bfc2b7a4850d16c94685b
gorohovart <gorohov.art@gmail.com>
2014-03-14 23:14:24 +0400

    Added n!

---
+++ x71/2nd semester/Task 2/Class/3. n!/Program.fs
@@ -0,0 +1,9 @@
+﻿let rec sum n = 
+    if n < 10I then n
+    else n % 10I + sum (n / 10I)
+let rec factorial n =
+    if n = 1 then 1I
+    else 
+        (System.Numerics.BigInteger n) * factorial (n - 1)
+
+printfn "%A" (sum (factorial 100))
