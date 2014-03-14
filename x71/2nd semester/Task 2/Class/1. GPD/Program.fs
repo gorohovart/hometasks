41c24be1f8ee954050ae167bbb50568755cf9b77
gorohovart <gorohov.art@gmail.com>
2014-03-14 23:13:33 +0400

    Added GPD

---
+++ x71/2nd semester/Task 2/Class/1. GPD/Program.fs
@@ -0,0 +1,10 @@
+﻿let GPD n =
+    let rec divide m (k : int64) =
+         if m = 1L then k
+         else if m % k = 0L then divide (m / k) k
+         else divide m (k + 1L)
+    divide n 2L
+
+printfn "%A" (GPD 600851475143L)
+
+
