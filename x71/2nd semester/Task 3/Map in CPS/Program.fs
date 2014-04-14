bcd9d1f33b4848a09b4cf347cc55376bd9a3a3e0
gorohovart <gorohov.art@gmail.com>
2014-04-14 14:44:40 +0400

    Added Map'CPS

---
+++ x71/2nd semester/Task 3/Map in CPS/Program.fs
@@ -0,0 +1,13 @@
+﻿// Gorokhov Artem (c) 2014
+// 2nd semester HW 3
+// Map in continuation passing style
+let rec listMapCPS f l g =
+    match l with
+    | hd :: tl -> f hd (fun x -> listMapCPS f tl (fun y -> g (x :: y)))
+    | [] -> g []
+
+let sqr' x f = f (x * x)
+
+let list = [1..10]
+
+listMapCPS sqr' list  (printfn "%A ")
