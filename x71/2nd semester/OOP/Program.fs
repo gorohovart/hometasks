64c3f687488794e8ddc997e49af6c63855670d70
Artem Gorohov <gorohov.art@gmail.com>
2014-04-25 00:26:32 +0400

    Added ages. Fixed bugs and solution

--- x71/2nd semester/OOP/Program.fs
+++ x71/2nd semester/OOP/Program.fs
@@ -1,6 +1,4 @@
-﻿module Cats
-
-let randomiseTF() =
+﻿let randomiseTF() =
     let rnd = new System.Random()
     if rnd.Next(2) = 0 then false
     else true
@@ -9,14 +7,13 @@ type Named(name:string) =
     member this.Name = name
 
 [<AbstractClass>]
-type Human(name:string) = 
+type Human(name:string, age:int) = 
     inherit Named(name)
 
 type Animal(name:string, isPet:bool) =
     inherit Named(name)
 
 type Cat(name:string, color:string, hasFur:bool) =
-    //class
     inherit Animal(name, true)
     let mutable mood = 5
     member this.Mood = mood 
@@ -34,12 +31,15 @@ type Cat(name:string, color:string, hasFur:bool) =
             if mood > 5 then "good"
             else if mood > 3 then "bad"
             else "terrible"
-    //end
 
-type Worker(name:string) = 
-    inherit Human(name)
+type Worker(name:string, age:int) = 
+    inherit Human(name, age)
     let mutable salary = 0
     let mutable cat = None
+    let mutable isDead = false
+    let mutable Age = age
+    member this.age = Age
+    member this.isAlive = not isDead
     member this.setPet p = cat <- p
     member this.getPet =
         cat <- Some(new Cat("Kot", "blue", true))
@@ -50,16 +50,23 @@ type Worker(name:string) =
                        printfn ("Grats!You have grown to %A $ salary!") this.getSalary
     member this.tryToGrow = if randomiseTF() then this.grow
     member this.year =
-        printfn ("You are a worker with %A $ salary now.") this.getSalary
-        if randomiseTF() && cat = None then this.getPet
+        if randomiseTF() then isDead <- true
+                              printfn ("Sorry, you died.")
+                         else
+                              Age <- (Age + 1)
+                              printfn ("Age %A. You are a worker with %A $ salary now.") this.age this.getSalary
+                              if randomiseTF() && cat = None then this.getPet
 
-type Student(name:string) = 
-    inherit Human(name)
+type Student(name:string, age:int) = 
+    inherit Human(name, age)
     let mutable courseNum = 1
     let mutable worker = None
     let mutable cat = None
+    let mutable Age = age
 
+    member this.age = Age
     member this.getWorker = match worker with |Some(x) -> x
+                                              |None -> failwith "Error"
 
     member this.isWorker = if worker = None then false
                            else true
@@ -70,7 +77,7 @@ type Student(name:string) =
         printfn ("You have got a cat now!")
 
     member this.becomeWorker =
-        let w = new Worker(name)
+        let w = new Worker(name, Age)
         if courseNum < 4 then w.setSalary(100 * courseNum)
         else w.setSalary(1000)
         w.setPet(cat)
@@ -80,34 +87,40 @@ type Student(name:string) =
     member this.exam = 
         let isPassed = randomiseTF()
         if isPassed && courseNum < 4 then courseNum <- (courseNum + 1)
+                                          printfn ("Grats!You have passed exam.")
         else this.becomeWorker
     member this.year =
-        printfn ("You are on %A course now.") courseNum
+        Age <- (Age + 1)
+        printfn ("Age %A. You are on %A course now.") this.age courseNum
         if randomiseTF() && cat = None then this.getPet
         this.exam
 
-type Schoolboy(name:string) = 
-    inherit Human(name)
+type Schoolboy(name:string, age:int) = 
+    inherit Human(name, age)
     let mutable classNum = 1
     let mutable student = None
     let mutable worker = None
     let mutable cat = None
+    let mutable Age = age
     
+    member this.age = Age
     member this.getWorker = match worker with |Some(x) -> x
+                                              |None -> failwith "Error"
     member this.getStudent = match student with |Some(x) -> x
+                                                |None -> failwith "Error"
 
     member this.isWorker = if worker = None then false
                            else true
     member this.isStudent = if student = None then false
                             else true
     member this.becomeStudent =
-        let s = new Student(name)
+        let s = new Student(name, Age)
         s.setPet(cat)
         student <- Some(s)
         printfn ("Grats!You have become a student with 0 salary! ;)")
 
     member this.becomeWorker =
-        let w = new Worker(name)
+        let w = new Worker(name, Age)
         if classNum < 11 then w.setSalary(1)
         else w.setSalary(10)
         w.setPet(cat)
@@ -128,7 +141,8 @@ type Schoolboy(name:string) =
              this.becomeWorker
 
     member this.year =
-        printfn ("You are in %A class now.") classNum
+        Age <- (Age + 1)
+        printfn ("Age %A. You are in %A class now.") this.age classNum
         let isWantPet = randomiseTF()
         if isWantPet && cat = None then this.getPet
         if classNum = 9 || classNum = 11 then this.exam
@@ -136,7 +150,7 @@ type Schoolboy(name:string) =
 
 
 let life =
-    let s = new Schoolboy("Vasya")
+    let s = new Schoolboy("Vasya", 6)
     
     while (not s.isWorker) && (not s.isStudent) do
         s.year
@@ -146,12 +160,12 @@ let life =
         while not st.isWorker do
             st.year
 
-        let w = s.getWorker
-        for i in [1..60] do
+        let w = st.getWorker
+        while w.isAlive do
             w.year
 
     else let w = s.getWorker
-         for i in [1..60] do
+         while w.isAlive do
              w.year
 
 life
