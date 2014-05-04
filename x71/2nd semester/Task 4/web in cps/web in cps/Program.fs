e75d5436e03bd364b48e4dc070b53d2bf6a6faf8
Artem Gorohov <gorohov.art@gmail.com>
2014-05-04 15:58:42 +0400

    Added web'cps

---
+++ x71/2nd semester/Task 4/web in cps/web in cps/Program.fs
@@ -0,0 +1,34 @@
+﻿module imgParse
+
+open WebR
+open System
+
+let urls =
+    [
+     "http://images.yandex.ru";
+     "http://google.com";
+    ]
+
+let imgNum (page:string) = 
+    let rec num (pos:int) =
+        let x = page.IndexOf ("<img", pos)
+        if x = -1 then 0
+                  else 1 + num (x + 4)
+    num 0
+
+let getImages (page:string) =
+    let rec get (pos:int) =
+        let x = page.IndexOf ("<img", pos)
+        if x = -1 then []
+                  else let y = page.IndexOf ("src=", x)
+                       let z = page.IndexOf ("\"", y + 5)
+                       [page.Substring (y + 5, z - y - 5)] :: get z
+    Seq.distinct (get 0) |> Seq.toList
+
+let rec parse urls g =
+    match urls with
+    | [] -> g []
+    | hd :: tl -> getUrl hd (fun x -> parse tl (fun y -> g ((if imgNum x < 6 then [] else getImages x) @ y)))
+                    
+parse urls (printfn "%A")
+Console.ReadLine()
