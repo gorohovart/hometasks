15ef359a909f3917507184ae23624f19dacdf432
Artem Gorohov <gorohovart@users.noreply.github.com>
2014-05-28 00:16:40 +0400

    Signature added

--- x71/2nd semester/Task 2/Class/2. sumFib/Program.fs
+++ x71/2nd semester/Task 2/Class/2. sumFib/Program.fs
@@ -1,4 +1,8 @@
-﻿let max = 4L * 1000L * 1000L
+// Gorokhov Artem (c) 2014
+// 2nd semester HW 2
+// Sum of even Fibonacci < 4.000.000
+
+let max = 4L * 1000L * 1000L
 
 let fibonachiSum = 
     let rec fib a b = 
