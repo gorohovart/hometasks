62453b7d92ad72d03c38af9bbafd0e9d8ce0da21
Artem Gorohov <gorohov.art@gmail.com>
2014-11-21 00:02:19 +0300

    Clouds added

---
+++ x71/3rd semester/Clouds/CloudTests/Library1.fs
@@ -0,0 +1,63 @@
+﻿module CloudTests.Tests
+ 
+open NUnit.Framework
+open NSubstitute
+open FsCheck
+open Clouds
+open FsUnit
+
+let test (isShining : bool, speed : int, dayLight : DayLightType, expectedCreature : CreatureType, expectedCourier : CourierType) =
+    let luminary = Substitute.For<ILuminary>()
+    let wind = Substitute.For<IWind>()
+    let dayLightMock = Substitute.For<IDayLight>()
+    let magic = Substitute.For<IMagic>()
+    let courier = Substitute.For<ICourier>()
+
+    ignore <| luminary.IsShining.Returns(isShining) 
+    ignore <| wind.Speed.Returns(speed)
+    ignore <| dayLightMock.Current.Returns(dayLight)
+    ignore <| magic.CallDaemon().Returns(courier)
+    ignore <| magic.CallStork().Returns(courier)
+
+    let creature = (new Cloud(dayLightMock, luminary, wind, magic)).Create()
+
+    (creature.getType) |> should equal expectedCreature
+    if expectedCourier = Stork then ignore <| magic.Received().CallStork()
+    else ignore <| magic.Received().CallDaemon()
+    
+    courier.Received().GiveBaby(creature)
+
+[<Test>] 
+let ``test 1`` () =  test (false, 1, DayLightType.Morning, CreatureType.Piglet, Stork)
+[<Test>] 
+let ``test 2`` () =  test (false, 1, DayLightType.Evening, CreatureType.Kitten, Daemon)
+[<Test>] 
+let ``test 3`` () =  test (false, 6, DayLightType.Morning, CreatureType.Piglet, Stork)
+[<Test>] 
+let ``test 4`` () =  test (false, 4, DayLightType.Night, CreatureType.Bat, Daemon)
+[<Test>] 
+let ``test 5`` () =  test (false, 10, DayLightType.Morning, CreatureType.Balloon, Stork)
+[<Test>] 
+let ``test 6`` () =  test (false, 6, DayLightType.Noon, CreatureType.Bearcub, Daemon)
+[<Test>] 
+let ``test 7`` () =  test (false, 0, DayLightType.Noon, CreatureType.Hedgehog, Stork)
+[<Test>] 
+let ``test 8`` () =  test (false, 0, DayLightType.Night, CreatureType.Hedgehog, Daemon)
+[<Test>] 
+let ``test 9`` () =  test (true, 1, DayLightType.Night, CreatureType.Piglet, Daemon)
+[<Test>] 
+let ``test 10`` () =  test (true, 1, DayLightType.Noon, CreatureType.Hedgehog, Daemon)
+[<Test>] 
+let ``test 11`` () =  test (true, 0, DayLightType.Evening, CreatureType.Kitten, Stork)
+[<Test>] 
+let ``test 12`` () =  test (true, 0, DayLightType.Night, CreatureType.Bat, Daemon)
+[<Test>] 
+let ``test 13`` () =  test (true, 0, DayLightType.Morning, CreatureType.Puppy, Stork)
+[<Test>] 
+let ``test 14`` () =  test (true, 10, DayLightType.Evening, CreatureType.Balloon, Stork)
+[<Test>] 
+let ``test 15`` () =  test (true, 1, DayLightType.Morning, CreatureType.Piglet, Stork)
+[<Test>] 
+let ``test 16`` () =  test (true, 6, DayLightType.Evening, CreatureType.Puppy, Stork)
+[<Test>] 
+let ``test 17`` () =  test (true, 9, DayLightType.Night, CreatureType.Bat, Daemon)
---
+++ x71/3rd semester/Clouds/Clouds/Program.fs
@@ -0,0 +1,113 @@
+﻿module Clouds
+
+
+let private rnd = new System.Random()
+
+type DayLightType = 
+    | Morning
+    | Noon
+    | Evening
+    | Night
+
+type CreatureType = 
+    | Puppy
+    | Kitten
+    | Hedgehog
+    | Bearcub
+    | Piglet
+    | Bat
+    | Balloon
+
+type CourierType = 
+    | Stork
+    | Daemon
+
+type Creature (creature : CreatureType) =
+    member x.getType() = creature //?
+
+type IDayLight =
+    abstract Current : DayLightType
+
+type ILuminary =
+    abstract IsShining : bool
+
+type IWind =
+    abstract Speed : int
+
+type ICourier =
+    abstract member GiveBaby : Creature -> Unit
+
+type IMagic =
+    abstract member CallStork : Unit -> ICourier
+    abstract member CallDaemon : Unit -> ICourier
+
+type Daylight () = 
+    interface IDayLight with
+        member x.Current = 
+            match rnd.Next(4) with 
+            | 0 -> DayLightType.Morning
+            | 1 -> DayLightType.Noon
+            | 2 -> DayLightType.Evening
+            | 3 -> DayLightType.Night
+            | _ -> failwith "wat??"
+
+type Luminary () = 
+    interface ILuminary with
+        member x.IsShining = rnd.Next(2) = 0
+
+type Wind () = 
+    interface IWind with
+        member x.Speed = rnd.Next(11)
+
+type Courier (t : CourierType) = 
+    interface ICourier with
+        member x.GiveBaby _ = ()  //???
+
+type Magic () = 
+    interface IMagic with  
+        member x.CallStork () = new Courier(CourierType.Stork) :> ICourier
+        member x.CallDaemon () = new Courier(CourierType.Daemon) :> ICourier
+
+type Cloud (dayLight: IDayLight, luminary : ILuminary, wind: IWind, magic: IMagic) =
+    member private x.InternalCreate() =
+        if luminary.IsShining then
+            match dayLight.Current with
+            | DayLightType.Morning -> if wind.Speed = 0 then    CreatureType.Hedgehog, magic.CallStork()
+                                      elif wind.Speed < 5 then  CreatureType.Kitten,   magic.CallStork()
+                                      elif wind.Speed < 10 then CreatureType.Piglet,   magic.CallStork()
+                                      else                      CreatureType.Balloon,  magic.CallDaemon()
+            | DayLightType.Noon    -> if wind.Speed = 0 then    CreatureType.Hedgehog, magic.CallStork()
+                                      elif wind.Speed < 5 then  CreatureType.Kitten,   magic.CallStork()
+                                      elif wind.Speed < 10 then CreatureType.Bearcub,  magic.CallStork()
+                                      else                      CreatureType.Balloon,  magic.CallDaemon()
+            | DayLightType.Evening -> if wind.Speed = 0 then    CreatureType.Hedgehog, magic.CallStork()
+                                      elif wind.Speed < 5 then  CreatureType.Kitten,   magic.CallDaemon()
+                                      elif wind.Speed < 10 then CreatureType.Piglet,   magic.CallStork()
+                                      else                      CreatureType.Balloon,  magic.CallDaemon()
+            | DayLightType.Night   -> if wind.Speed = 0 then    CreatureType.Hedgehog, magic.CallDaemon()
+                                      elif wind.Speed < 10 then CreatureType.Bat,      magic.CallDaemon()
+                                      else                      CreatureType.Balloon,  magic.CallDaemon()
+        else
+            match dayLight.Current with
+            | DayLightType.Morning -> if wind.Speed = 0 then    CreatureType.Puppy,    magic.CallStork()
+                                      elif wind.Speed < 6 then  CreatureType.Piglet,   magic.CallStork()
+                                      elif wind.Speed < 10 then CreatureType.Puppy,    magic.CallStork()
+                                      else                      CreatureType.Balloon,  magic.CallStork()
+            | DayLightType.Noon    -> if wind.Speed = 0 then    CreatureType.Kitten,   magic.CallStork()
+                                      elif wind.Speed < 6 then  CreatureType.Hedgehog, magic.CallDaemon()
+                                      elif wind.Speed < 10 then CreatureType.Puppy,    magic.CallStork()
+                                      else                      CreatureType.Balloon,  magic.CallStork()
+            | DayLightType.Evening -> if wind.Speed = 0 then    CreatureType.Kitten,   magic.CallStork()
+                                      elif wind.Speed < 6 then  CreatureType.Hedgehog, magic.CallDaemon()
+                                      elif wind.Speed < 10 then CreatureType.Puppy,    magic.CallStork()
+                                      else                      CreatureType.Balloon,  magic.CallStork()
+            | DayLightType.Night   -> if wind.Speed = 0 then    CreatureType.Bat,      magic.CallDaemon()
+                                      elif wind.Speed < 6 then  CreatureType.Piglet,   magic.CallDaemon()
+                                      elif wind.Speed < 10 then CreatureType.Bat,      magic.CallStork()
+                                      else                      CreatureType.Balloon,  magic.CallStork()
+ 
+    member x.Create() =
+      let creatureType, cour = x.InternalCreate()
+      let creature = new Creature(creatureType)   
+      cour.GiveBaby(creature)
+      creature
