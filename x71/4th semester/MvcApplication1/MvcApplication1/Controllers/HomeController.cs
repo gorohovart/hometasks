8d1e56053c0d3b5e1817eb1db75c4d25dc338b6e
Artem Gorohov <gorohov.art@gmail.com>
2015-04-26 20:50:42 +0300

    Mvc added

---
+++ x71/4th semester/MvcApplication1/MvcApplication1/Controllers/HomeController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System.Configuration;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace MvcApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Index2()
+        {
+            Session["Index"] = -1;
+            return View();
+        }
+        public ActionResult StartView()
+        {
+            Models.Picture[] pictures = (Models.Picture[])Session["Pictures"];
+            int index = 0;
+            if (Session["Index"] != null)
+            {
+                index = (int)Session["Index"];
+
+                if (index == pictures.Length)
+                {
+                    return View("WinView");
+                }
+            }
+
+            if (pictures == null)
+            {
+                pictures = GetAll();
+                index = 0;
+                Session.Add("Pictures", pictures);
+                Session["Index"] = index;
+            }
+            else if (index == -1)
+            {
+                index = 0;
+                Session["Index"] = index;
+            }
+
+            //ViewBag.Counter = index + 1;
+            return View(pictures[index]);
+        }
+        public ActionResult Guess(string type)
+        {
+            Models.Picture[] pictures = (Models.Picture[])Session["Pictures"];
+            int index = (int)Session["Index"];
+
+            if (pictures[index].Type.Equals(type))
+            {
+                Session["Index"] = ((int)Session["Index"] + 1);
+                return View("RightGuess", pictures[index]);
+            }
+            else
+            {
+                Session["Index"] = -1;
+                return View("WrongGuess", pictures[index]);
+            }
+        }
+        private Models.Picture[] GetAll()
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+                 ConfigurationManager.ConnectionStrings["DataConnectionString"].ConnectionString);
+
+            CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
+            CloudTable table = tableClient.GetTableReference("image");
+            TableQuery<Models.Picture> query = new TableQuery<Models.Picture>();
+
+            return table.ExecuteQuery(query).ToArray();
+        }
+
+
+    }
+}
---
+++ x71/4th semester/MvcApplication1/MvcApplication1/Models/Picture.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Table;
+using System.Configuration;
+
+namespace MvcApplication1.Models
+{
+     
+    public class Picture : TableEntity
+    {
+        public int Number { get; set; }
+        public Picture(string num1, string num2)
+        {
+            this.PartitionKey = num1;
+            this.RowKey = num2;
+        }
+        public Picture() { }
+        public Boolean isPorn { get; set; }
+        public string Type { get; set; }
+        public string url { get; set; }
+        public string urlFull { get; set; }
+    }
+}
