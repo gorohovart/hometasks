a7bcd2f956d5ab16f218de32a2c24f9220ebfd8f
Artem Gorohov <gorohov.art@gmail.com>
2016-01-07 18:05:55 +0300

    fix

--- x71/5th semester/Computer Vision/2/3. perspective & orthographic images/perspective & orthographic images.py
+++ x71/5th semester/Computer Vision/2/3. perspective & orthographic images/perspective & orthographic images.py
@@ -20,21 +20,21 @@ def perspectiveProjection(point, cx, angle):
 
 def orthographicProjection(points):
     mat = np.asmatrix(points)
-    mat2 = np.asmatrix(np.array([[int(imgX/2), 0, 0, 0],
-                                 [0, int(imgY/2), 0, 0],
+    mat2 = np.asmatrix(np.array([[int(imgX/4), 0, 0, 0],
+                                 [0, int(imgY/4), 0, 0],
                                  [0, 0, 0, 0],
                                  [int(imgX/2), int(imgY/2), 0, 1]]))
     return mat * mat2
 
+tmp1 = math.sqrt((math.sqrt(1.5 ** 2 + 2.5 ** 2) * 2) ** 2 - 8)
+sq_side = tmp1 - math.sqrt(2)
 
-sq_side = math.sqrt(32) - math.sqrt(8)/2
+tmp = math.sqrt(((tmp1 - math.sqrt(8))**2)/2)
 
-tmp1 = math.sqrt(((math.sqrt(32) - math.sqrt(8))**2)/2)
-
-pointsOfSquare = np.array([[sq_side/2, 1, 3, 1],
-                           [-1 * sq_side/2, 1, 3, 1],
-                           [sq_side/2, -1 * tmp1, 4 + tmp1, 1],
-                           [-1 * sq_side/2, -1 * tmp1, 4 + tmp1, 1]])
+pointsOfSquare = np.array([[sq_side/2, 1.5, 2.5, 1],
+                           [-1 * sq_side/2, 1.5, 2.5, 1],
+                           [sq_side/2, -1 * tmp, 4 + tmp, 1],
+                           [-1 * sq_side/2, -1 * tmp, 4 + tmp, 1]])
 imgX = 700
 imgY = imgX
 
@@ -68,6 +68,5 @@ cv2.line(img, projections[3], projections[2], [0, 0, 0])
 cv2.line(img, projections[2], projections[0], [0, 0, 0])
 
 cv2.imshow('Orthographic', img)
-# print(math.sqrt((math.sqrt(1.5 ** 2 + 2.5 ** 2) * 2) ** 2 - 8) * math.sqrt(2))
 cv2.waitKey(0)
 cv2.destroyAllWindows()
