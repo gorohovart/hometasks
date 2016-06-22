452a29f8e2d49950ecd1fe8f60af37498ad85f63
Artem Gorohov <gorohov.art@gmail.com>
2016-06-22 21:05:21 +0300

    Parse of vertical input added. + Test for it.

--- x71/6th semester/lambda/lambda/Program.fs
+++ x71/6th semester/lambda/lambda/Program.fs
@@ -30,10 +30,32 @@ let rec reduction = function
     | App (A, B) -> reduction <| App(reduction A, reduction B)
     | c -> c
 
+let readInput path =
+    let rec parseInput = function
+        | (hd:string) :: tl -> 
+            if hd.Contains "\\" then
+                let A, toParse = parseInput tl
+                (L A), toParse
+            elif hd.Contains "@" then
+                let A, toParse = parseInput tl
+                match toParse with
+                | [] -> failwith "Wrong number of args in application(< 2)."
+                | _ -> let B, toParse2 = parseInput toParse
+                       (App(A, B)), toParse2
+            else (N(int hd)), tl
+        | [] -> failwith "Wrong number of args."
+    let inputList = Seq.toList <| File.ReadAllLines path
+    let result, tail = parseInput inputList
+    match tail with
+    | [] -> result
+    | _ -> failwith "Too much args."
+
 [<EntryPoint>]
 let main argv = 
+    let path = argv.[0]
     let ast1 = L(App(L(App (N 1, N 0)), N 1))
-    let ast = L(L(L(App( App( L(L(App( App(N 2, N 1), N 0))), N 1 ), N 2 ))))
+    let ast2 = L(L(L(App( App( L(L(App( App(N 2, N 1), N 0))), N 1 ), N 2 ))))
+    let ast = readInput path
     let reduced = reduction ast
 
     printfn "%A" (ast.ToString())
