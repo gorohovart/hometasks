1a95c50eeb9a00d371316d4efa35c06e76a076f9
Artem Gorohov <gorohov.art@gmail.com>
2016-06-23 00:31:24 +0300

    Fix of substitution + new test.

--- x71/6th semester/lambda/lambda/Program.fs
+++ x71/6th semester/lambda/lambda/Program.fs
@@ -9,9 +9,9 @@ type Tree =
         match this with
         | N n -> n.ToString()
         | L A -> "\." + A.ToString()
-        | App (L A, App (B, C)) -> "(" + A.ToString() + ") (" + B.ToString() + ")"
-        | App (A, App (B, C)) -> A.ToString() + " (" + B.ToString() + ")"
-        | App (L A, B) -> "(" + A.ToString() + ") " + B.ToString()
+        | App (L A, App (B, C)) -> "(" + (L A).ToString() + ") (" + (App(B, C)).ToString() + ")"
+        | App (A, App (B, C)) -> A.ToString() + " (" + (App(B, C)).ToString() + ")"
+        | App (L A, B) -> "(" + (L A).ToString() + ") " + B.ToString()
         | App (A, B) -> A.ToString() + " " + B.ToString()
 
 let rec shift (N c) (N i) = function
@@ -19,13 +19,14 @@ let rec shift (N c) (N i) = function
     | L A -> L (shift (N(c + 1)) (N i) A)
     | App (A, B) -> App (shift (N c) (N i) A, shift (N c) (N i) B)
 
-let rec substitution (N m) (N e) = function
-    | N n -> N (if n = m then e else n)
-    | L A -> L (substitution (N (m+1)) (shift (N 0) (N 1) (N e)) A)
-    | App (A, B) -> App(substitution (N m) (N e) A, substitution (N m) (N e) B)
+let rec substitution (N m) e = function
+    | N n -> if n = m then e else (N n)
+    | L A -> L (substitution (N (m+1)) (shift (N 0) (N 1) e) A)
+    | App (A, B) -> App(substitution (N m) e A, substitution (N m) e B)
 
 let rec reduction = function
-    | App ((L A), B) -> shift (N 0) (N -1) (substitution (N 0) (shift (N 0) (N 1) B) A)
+    | App ((L A), B) -> let c = B
+                        shift (N 0) (N -1) (substitution (N 0) (shift (N 0) (N 1) B) A)
     | L t -> L(reduction t)
     | App (A, B) -> reduction <| App(reduction A, reduction B)
     | c -> c
@@ -49,15 +50,22 @@ let readInput path =
     match tail with
     | [] -> result
     | _ -> failwith "Too much args."
+let writeToFile path ast =
+    let rec astToList = function
+    | N n -> [n.ToString()] 
+    | L A -> "\\"::(astToList A)
+    | App (A, B) -> "@"::(astToList A)@(astToList B)
 
+    File.WriteAllLines (path, astToList ast)
+        
 [<EntryPoint>]
 let main argv = 
     let path = argv.[0]
     let ast1 = L(App(L(App (N 1, N 0)), N 1))
     let ast2 = L(L(L(App( App( L(L(App( App(N 2, N 1), N 0))), N 1 ), N 2 ))))
     let ast = readInput path
+    printfn "Your input:\n%s" (ast.ToString())
     let reduced = reduction ast
-
-    printfn "%A" (ast.ToString())
-    printfn "%A" (reduced.ToString())
+    printfn "Reduced:\n%s" (reduced.ToString())
+    writeToFile "result.txt" reduced
     0
