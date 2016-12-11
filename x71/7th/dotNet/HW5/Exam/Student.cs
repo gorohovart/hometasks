a5d26e793b794d0182efc86f598d98d27aeb57ee
gorohovart <gorohov.art@gmail.com>
2016-12-11 20:16:01 +0300

    hw5

---
+++ x71/7th/dotNet/HW5/Exam/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam
+{
+    public sealed class Student
+    {
+        public string Name { get; }
+        public int Mark { get; private set; }
+
+        public void PassExam(int mark)
+        {
+            Mark = mark;
+        }
+    }
+}
